fc84fb95e860df5fa6264543a0ca9fdb79bfdb2c
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-05-08 16:23:57 +0530

    Updated styles as per mobile

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import ChatScreen from "./components/ChatScreen/ChatScreen";
 import ChatSidebar from "./components/ChatSidebar/ChatSidebar";
 import Login from "./components/Login/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "./contexts/UserContext";
 import "./App.css";
 
 function App() {
   const { user } = useContext(UserContext);
+  const [show, setShow] = useState(true);
+
+  const clickHandler = () => {
+    setShow(!show);
+  };
 
   return !user ? (
     <Login />
   ) : (
     <div className="container">
       <Router>
-        <ChatSidebar />
+        <ChatSidebar show={show} clickHandler={clickHandler} />
         <Switch>
           <Route path="/rooms/:roomId">
-            <ChatScreen />
+            <ChatScreen show={show} clickHandler={clickHandler} />
           </Route>
         </Switch>
       </Router>
